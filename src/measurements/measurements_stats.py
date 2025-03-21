21d7d734336eb1301e5f89bb0265b0fc12edcf72
StEn <github@steven-england.info>
2025-03-21 14:50:47 +0100

    refactor: initialize statistics attributes to zero and compute values conditionally

--- src/measurements/measurements_stats.py
+++ src/measurements/measurements_stats.py
@@ -4,11 +4,16 @@ import statistics
 class MeasurementsStats:  # noqa: WPS230
     def __init__(self, measurements: list[float], failed_attempts: int = 0) -> None:
         self.latencies = measurements
-        self.min = min(self.latencies)
-        self.max = max(self.latencies)
-        self.mean = statistics.mean(self.latencies)
-        self.median = statistics.median(self.latencies)
+        self.min: float = 0
+        self.max: float = 0
+        self.mean: float = 0
+        self.median: float = 0
         self.stdev: float = 0
+        if self.latencies:
+            self.min = min(self.latencies)
+            self.max = max(self.latencies)
+            self.mean = statistics.mean(self.latencies)
+            self.median = statistics.median(self.latencies)
         if len(self.latencies) > 1:
             self.stdev = statistics.stdev(self.latencies)
 
